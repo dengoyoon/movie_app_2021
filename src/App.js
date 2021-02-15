22dac5e59fa13a79c2f87de7a9f8486bb371b859
윤두현 <duhyeon@yunduhyeon-ui-MacBookPro.local>
2021-02-15 16:04:41 +0900

    React UPDATE 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function component
+// function App() {
+//   return (
+//     <div> 
+//       <h1>Hello!!</h1>
+//       {foodILike.map(renderFood)}
+//     </div>
+//   );
+// }
+
+// class component , for 'state'
+class App extends React.Component{
+  state = {
+    isLoading: true
+  };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading : false});
+    }, 6000);
+  }
+  //'render()' is only function in React component
+  render() {  // React excutes render method that my class component have
+    const { isLoading } = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading" : "We are ready"}
+      </div>
+    );
+  }
 }
 
 export default App;
